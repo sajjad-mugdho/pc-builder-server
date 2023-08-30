a08536b6e303dd3a4faf5a8a42981e1f09f32164
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-08-31 02:57:17 +0600

    final

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,7 +4,7 @@ import { ProductController } from "./controller";
 const router = express.Router();
 router.get("/last", ProductController.getLast);
 router.get("/", ProductController.getAllProducts);
-router.get("/:category", ProductController.getProductsByCategory);
+router.get("/pc/:category", ProductController.getProductsByCategory);
 router.get("/:id", ProductController.getProductById);
 router.post("/", ProductController.createProduct);
 router.patch("/:id", ProductController.updateProduct);
